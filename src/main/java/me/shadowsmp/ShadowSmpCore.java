0dd4169285c2cb7c44ad371631f5909b07493709
saifsifat2810-bit <saifsifat2810@gmail.com>
2026-09-18 20:05:58 +0600

    Update ShadowSmpCore.java

--- src/main/java/me/shadowsmp/ShadowSmpCore.java
+++ src/main/java/me/shadowsmp/ShadowSmpCore.java
@@ -4,8 +4,37 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShadowSmpCore extends JavaPlugin {
 
+    private HeartManager heartManager;
+    private LifestealManager lifestealManager;
+    private HeadManager headManager;
+
     @Override
     public void onEnable() {
+
+        heartManager = new HeartManager(this);
+        lifestealManager = new LifestealManager(heartManager);
+        headManager = new HeadManager(this);
+
+        getServer().getPluginManager().registerEvents(
+                heartManager,
+                this
+        );
+
+        getServer().getPluginManager().registerEvents(
+                lifestealManager,
+                this
+        );
+
+        getServer().getPluginManager().registerEvents(
+                headManager,
+                this
+        );
+
+        if (getCommand("withdraw") != null) {
+            getCommand("withdraw")
+                    .setExecutor(lifestealManager);
+        }
+
         getLogger().info("ShadowSmpCore enabled!");
     }
 
@@ -13,4 +42,4 @@ public class ShadowSmpCore extends JavaPlugin {
     public void onDisable() {
         getLogger().info("ShadowSmpCore disabled!");
     }
-}
+} 
